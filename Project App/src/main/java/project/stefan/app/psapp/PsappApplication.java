e16893ae7b236245128c0a7be70845e393ee9ccd
Stefan <cvetanovskistefan72@gmail.com>
2020-04-26 12:47:04 +0200

    first

---
+++ Project App/src/main/java/project/stefan/app/psapp/PsappApplication.java
@@ -0,0 +1,13 @@
+package project.stefan.app.psapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsappApplication.class, args);
+    }
+
+}
---
+++ Project App/src/test/java/project/stefan/app/psapp/PsappApplicationTests.java
@@ -0,0 +1,13 @@
+package project.stefan.app.psapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
