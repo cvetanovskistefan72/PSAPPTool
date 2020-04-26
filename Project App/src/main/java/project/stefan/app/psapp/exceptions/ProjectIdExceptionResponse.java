20df59fc12d71b139f9835a2a32eb7bcfec75171
Stefan <cvetanovskistefan72@gmail.com>
2020-04-26 21:44:10 +0200

    "Project ID Exception"

---
+++ Project App/src/main/java/project/stefan/app/psapp/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,10 @@
+package project.stefan.app.psapp.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    public String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier){
+        this.projectIdentifier=projectIdentifier;
+    }
+}
--- Project App/src/main/java/project/stefan/app/psapp/services/ProjectService.java
+++ Project App/src/main/java/project/stefan/app/psapp/services/ProjectService.java
@@ -4,6 +4,7 @@ package project.stefan.app.psapp.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.stefan.app.psapp.domain.Project;
+import project.stefan.app.psapp.exceptions.ProjectIdException;
 import project.stefan.app.psapp.repositories.ProjectRepository;
 
 @Service
@@ -13,7 +14,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID "+project.getProjectIdentifier().toUpperCase()+" already exists");
+        }
 
-        return projectRepository.save(project);
     }
 }
